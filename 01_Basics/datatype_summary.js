73a25c2f8c7a8880582a3a7466fe6e196e0b8cd8
Ronak <ronakkadam708@gmail.com>
2025-01-18 00:46:04 +0530

    understanding datatypes

---
+++ 01_Basics/datatype_summary.js
@@ -0,0 +1,8 @@
+//#Primitive Data Types (Call by value)
+
+// 7 types of primitive data types =>
+    // Number, String, Boolean, Null, Undefined, Symbol, BigInt
+
+// Reference types(Non primitive)
+
+// Array, Object, Function, Set, Map, Date, RegExp
