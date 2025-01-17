3e6d42fc53849b6a1dfe798f6ab6af70a16861bf
ronak7470 <ronakkadam708@gmail.com>
2025-01-17 13:00:24 +0000

    Learned new datatypes in javascript

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 344
+let accountEmail = "ronak@google.com"
+var accountPassword = "12345"
+accountCity = "Indore"
+let accountState;
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity, accountState])
+//prefer not to use var because of issue in block scope and functional scope
